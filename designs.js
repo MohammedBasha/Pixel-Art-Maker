4b0d397bf9d40799a4685c28e0083678c0b2c857
Mohammed Basha <mohammedbasha@bestresponsemedia.com>
2019-10-25 10:51:11 +0200

    Adding variables for the wanted elements

--- designs.js
+++ designs.js
@@ -1,3 +1,11 @@
+// Creating variables of the elements
+const SIZE_PICKER = document.getElementById("sizePicker"),
+      INPUT_HEIGHT = document.getElementById("inputHeight"),
+      INPUT_WIDTH = document.getElementById("inputWidth"),
+      SUBMIT_BTN = document.getElementById("submitBtn"),
+      COLOR_PICKER = document.getElementById("colorPicker"),
+      PIXEL_CANVAS = document.getElementById("pixelCanvas");
+
 // Select color input
 // Select size input
 
